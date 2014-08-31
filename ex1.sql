97bf3342fad4e5bdd00c509e791d812278a75501
clay-reed-a <clay.reed.a@gmail.com>
2014-08-30 20:04:13 -0400

    explore case-insensitive of sql

--- ex1.sql
+++ ex1.sql
@@ -1,6 +1,6 @@
 CREATE TABLE person (
-	id INTEGER PRIMARY KEY, 
-	first_name TEXT, 
-	last_name TEXT, 
-	age INTEGER 
+	id integer primary key, 
+	first_name text, 
+	last_name text, 
+	age integer 
 );
