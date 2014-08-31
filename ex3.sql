959fe132b18571d6ab17a79e1cbf43b6ff40d852
clay-reed-a <clay.reed.a@gmail.com>
2014-08-30 20:53:28 -0400

    "sqlite thought the table was too small"

--- ex3.sql
+++ ex3.sql
@@ -17,5 +17,12 @@ CREATE TABLE pet (
 INSERT INTO person (id, first_name, last_name, age)
 	VALUES (0, "Clayton", "Albachten", 27); 
 
-INSERT INTO pet (id, name, breed, age, dead)
+INSERT INTO pet (id, name, breed, age, dead, person_id)
 	VALUES (0, "Fritz", "Weimaraner", 70, 1, 0); 
+
+-- forgot "person_id" in the parameter parenthesis & so 
+-- Error: near line 20: 6 values for 5 columns
+-- Thought for nearly half a minute this was because 
+-- "sqlite thought the table was too small." 
+-- Even went in & looked at .schema
+-- Stupid! 
